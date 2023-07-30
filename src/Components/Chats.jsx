7224708bd2312369689fb4a0944e7803b996b409
Amartya Kumar <amartya324@gmail.com>
2023-07-30 23:48:48 +0530

    added optional chaining in Chats component

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -30,21 +30,22 @@ const Chats = () => {
 
   return (
     <div className="chats">
-      {Object.entries(chats)
-        ?.sort((a, b) => b[1].date - a[1].date)
-        .map((chat) => (
-          <div
-            className="userChat"
-            key={chat[0]}
-            onClick={() => handleSelect(chat[1].userInfo)}
-          >
-            <img src={chat[1].userInfo.photoURL} alt="" />
-            <div className="userChatInfo">
-              <span>{chat[1].userInfo.displayName}</span>
-              <p>{chat[1].lastMessage?.text}</p>
+      {chats?.length >
+        Object.entries(chats)
+          ?.sort((a, b) => b[1].date - a[1].date)
+          .map((chat) => (
+            <div
+              className="userChat"
+              key={chat[0]}
+              onClick={() => handleSelect(chat[1].userInfo)}
+            >
+              <img src={chat[1].userInfo.photoURL} alt="" />
+              <div className="userChatInfo">
+                <span>{chat[1].userInfo.displayName}</span>
+                <p>{chat[1].lastMessage?.text}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
     </div>
   );
 };
