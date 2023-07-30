392473583cb58a001d069b8cbd70e2951a221a95
Amartya Kumar <amartya324@gmail.com>
2023-07-31 00:07:36 +0530

    revert back the previous change in Chat component

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -30,22 +30,21 @@ const Chats = () => {
 
   return (
     <div className="chats">
-      {chats?.length >
-        Object.entries(chats)
-          ?.sort((a, b) => b[1].date - a[1].date)
-          .map((chat) => (
-            <div
-              className="userChat"
-              key={chat[0]}
-              onClick={() => handleSelect(chat[1].userInfo)}
-            >
-              <img src={chat[1].userInfo.photoURL} alt="" />
-              <div className="userChatInfo">
-                <span>{chat[1].userInfo.displayName}</span>
-                <p>{chat[1].lastMessage?.text}</p>
-              </div>
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1].userInfo)}
+          >
+            <img src={chat[1].userInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
             </div>
-          ))}
+          </div>
+        ))}
     </div>
   );
 };
